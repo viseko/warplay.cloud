be27e1df5f2ff48607f123a9b8837d5bf7263c07
Viseko <viseko23@gmail.com>
2022-04-24 01:17:04 +0700

    Вёрстка мобильного меню

--- src/js/app.js
+++ src/js/app.js
@@ -11,23 +11,23 @@ adaptiveMove(); // - адаптивный перенос элементов
 let isMenuOpened = false;
 
 // ** МОБИЛЬНОЕ МЕНЮ
-// const menuBtn = document.querySelector("#js-menu-btn");
-// const menuCloseBtn = document.querySelector("#js-menu-close");
+const menuBtn = document.querySelector(".js-menu-btn");
+const menuCloseBtn = document.querySelector(".js-menu-close");
 
-// function closeMenu() {
-//   document.documentElement.classList.remove("_menu-open");
-//   menuBtn.focus();
-//   isMenuOpened = false;
-// }
+function closeMenu() {
+  document.documentElement.classList.remove("_menu-open");
+  menuBtn.focus();
+  isMenuOpened = false;
+}
 
-// function openMenu() {
-//   document.documentElement.classList.add("_menu-open");
-//   menuCloseBtn.focus();
-//   isMenuOpened = true;
-// }
+function openMenu() {
+  document.documentElement.classList.add("_menu-open");
+  menuCloseBtn.focus();
+  isMenuOpened = true;
+}
 
-// menuBtn.addEventListener("click", openMenu);
-// menuCloseBtn.addEventListener("click", closeMenu);
+menuBtn.addEventListener("click", openMenu);
+menuCloseBtn.addEventListener("click", closeMenu);
 
 // ** ОВЕРЛЕЙ
 // const overlay = document.querySelector("#js-page-overlay");
